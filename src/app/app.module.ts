700e3ea399f888efba9d84c5c2aadf13766264e6
Teri11 <augustineteri157@gmail.com>
2022-12-08 10:52:55 +0300

    added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from './common/common.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    routingComponents,
     
   ],
   imports: [
